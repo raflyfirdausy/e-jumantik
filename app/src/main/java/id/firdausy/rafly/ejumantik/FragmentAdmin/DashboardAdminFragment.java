79fe8781c39731cbf2b0e8142a24352ee8e64bff
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2019-08-19 12:31:59 +0700

    SELESE

--- app/src/main/java/id/firdausy/rafly/ejumantik/FragmentAdmin/DashboardAdminFragment.java
+++ app/src/main/java/id/firdausy/rafly/ejumantik/FragmentAdmin/DashboardAdminFragment.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import id.firdausy.rafly.ejumantik.Helper.Bantuan;
 import id.firdausy.rafly.ejumantik.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DashboardAdminFragment extends Fragment {
+
+    private TextView tv_totalAdmin;
+    private TextView tv_totalKader;
+    private TextView tv_totalData;
+    private DatabaseReference databaseReference;
 
 
     public DashboardAdminFragment() {
@@ -25,7 +39,70 @@ public class DashboardAdminFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard_admin, container, false);
+        View v = inflater.inflate(R.layout.fragment_dashboard_admin, container, false);
+        tv_totalAdmin = v.findViewById(R.id.tv_totalAdmin);
+        tv_totalKader = v.findViewById(R.id.tv_totalKader);
+        tv_totalData = v.findViewById(R.id.tv_totalData);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        getTotalAdmin();
+        getTotalKader();
+        getTotalData();
+        return v;
+    }
+
+    private void getTotalData() {
+        databaseReference.child("input")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            int totalData = 0;
+                            for (DataSnapshot data : dataSnapshot.getChildren()) {
+                                totalData += data.getChildrenCount();
+                            }
+                            tv_totalData.setText(totalData + " Data");
+                        } else {
+                            tv_totalData.setText("0 Data");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        new Bantuan(getActivity()).swal_error(databaseError.getMessage());
+                    }
+                });
+    }
+
+    private void getTotalKader() {
+        databaseReference.child("user")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        tv_totalKader.setText(dataSnapshot.getChildrenCount() + " Kader");
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        new Bantuan(getActivity()).swal_error(databaseError.getMessage());
+                    }
+                });
+    }
+
+    private void getTotalAdmin() {
+        databaseReference.child("admin")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        tv_totalAdmin.setText(dataSnapshot.getChildrenCount() + " Admin");
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        new Bantuan(getActivity()).swal_error(databaseError.getMessage());
+                    }
+                });
     }
 
 }
--- app/src/main/java/id/firdausy/rafly/ejumantik/FragmentAdmin/HitungAbjAdminFragment.java
+++ app/src/main/java/id/firdausy/rafly/ejumantik/FragmentAdmin/HitungAbjAdminFragment.java
@@ -5,9 +5,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.firdausy.rafly.ejumantik.Helper.Bantuan;
+import id.firdausy.rafly.ejumantik.Helper.Waktu;
 import id.firdausy.rafly.ejumantik.R;
 
 /**
@@ -15,6 +29,18 @@ import id.firdausy.rafly.ejumantik.R;
  */
 public class HitungAbjAdminFragment extends Fragment {
 
+    private MaterialSpinner msBulan;
+    private MaterialSpinner msTahun;
+    private TextView tvPersen;
+    private TextView tvTidakDitemukanJentik;
+    private TextView tvTotalRumahDiPeriksa;
+    private DatabaseReference databaseReference;
+    private int bulanFix, tahunFix;
+
+    private String[] namaBulan = {"Januari", "Februari", "Maret", "April", "Mei", "Juni",
+            "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
+
+    private List<String> tahun = new ArrayList<>();
 
     public HitungAbjAdminFragment() {
         // Required empty public constructor
@@ -25,7 +51,120 @@ public class HitungAbjAdminFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hitung_abj_admin, container, false);
+        View v = inflater.inflate(R.layout.fragment_hitung_abj_admin, container, false);
+        msBulan = v.findViewById(R.id.msBulan);
+        msTahun = v.findViewById(R.id.msTahun);
+        tvPersen = v.findViewById(R.id.tvPersen);
+        tvTidakDitemukanJentik = v.findViewById(R.id.tvTidakDitemukanJentik);
+        tvTotalRumahDiPeriksa = v.findViewById(R.id.tvTotalRumahDiPeriksa);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        msBulan.setItems(namaBulan);
+
+        gettahun();
+
+        return v;
+    }
+
+
+    private void getData(final long bulan, final long tahun) {
+        databaseReference.child("input")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        long tidakDitemukan = 0;
+                        long rumahDiperiksa = 0;
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot data : dataSnapshot.getChildren()) {
+                                for (DataSnapshot dataLagi : data.getChildren()) {
+                                    String tahunString = String.valueOf(new Waktu(getActivity()).getTahunTimestamp(Long.parseLong(dataLagi.child("waktu").getValue(String.class))));
+                                    String bulanString = String.valueOf(new Waktu(getActivity()).getBulanTimestamp(Long.parseLong(dataLagi.child("waktu").getValue(String.class))));
+
+                                    long tahunLong = Long.parseLong(tahunString);
+                                    long bulanLong = Long.parseLong(bulanString);
+
+                                    if (bulan == bulanLong && tahun == tahunLong) {
+                                        rumahDiperiksa += 1;
+                                        if (dataLagi.child("totalPositifJentik").getValue(String.class).equalsIgnoreCase("0")) {
+                                            tidakDitemukan += 1;
+                                        }
+                                    }
+                                }
+                            }
+                            float persen = 0;
+                            if (rumahDiperiksa != 0) {
+                                persen = ((float) tidakDitemukan) / rumahDiperiksa * 100;
+                            }
+
+                            tvPersen.setText(String.valueOf(String.format("%.2f", persen)));
+                            tvTidakDitemukanJentik.setText("Total Rumah Tidak Ditemukan Jentik : " + tidakDitemukan);
+                            tvTotalRumahDiPeriksa.setText("Total Rumah yang di periksa : " + rumahDiperiksa);
+                        } else {
+                            new Bantuan(getActivity()).swal_error("Data tidak ditemukan");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        new Bantuan(getActivity()).swal_error(databaseError.getMessage());
+                    }
+                });
+    }
+
+    private void gettahun() {
+        databaseReference.child("input")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        tahun.clear();
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot data : dataSnapshot.getChildren()) {
+                                for (DataSnapshot dataLagi : data.getChildren()) {
+                                    String tahunString = String.valueOf(new Waktu(getActivity()).getTahunTimestamp(Long.parseLong(dataLagi.child("waktu").getValue(String.class))));
+                                    boolean kosong = true;
+                                    for (int i = 0; i < tahun.size(); i++) {
+                                        if (tahun.get(i).contains(tahunString)) {
+                                            kosong = false;
+                                            break;
+                                        }
+                                    }
+
+                                    if (kosong) {
+                                        tahun.add(tahunString);
+                                    }
+                                }
+                            }
+                        }
+                        msTahun.setItems(tahun);
+                        tahunFix = Integer.parseInt(tahun.get(0));
+                        getData(bulanFix, tahunFix);
+
+                        msBulan.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+//                new Bantuan(getActivity()).swal_sukses(String.valueOf(view.getSelectedIndex()));
+                                bulanFix = view.getSelectedIndex();
+                                getData(bulanFix, tahunFix);
+                            }
+                        });
+
+
+                        msTahun.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+//                new Bantuan(getActivity()).swal_sukses(view.getItems().get(position).toString());
+                                tahunFix = Integer.parseInt(view.getItems().get(position).toString());
+                                getData(bulanFix, tahunFix);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        new Bantuan(getActivity()).swal_error(databaseError.getMessage());
+                    }
+                });
     }
 
 }
